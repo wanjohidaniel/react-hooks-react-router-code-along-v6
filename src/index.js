fe84daecf3bd80be8900a633b051e38e67ba11a0
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-05-01 00:29:41 +0300

    React Router Code-Along

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import UserProfile from "./components/UserProfile";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }, 
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/profile/:id",
+    element: <UserProfile />,
+    errorElement: <ErrorPage />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
